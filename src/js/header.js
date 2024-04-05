7563ba2a425db27c8cc812e6d7a4afee3246a419
Admin <admin@MacBook-Air.local>
2024-04-05 21:31:51 +0300

    add form

--- src/js/header.js
+++ src/js/header.js
@@ -3,11 +3,17 @@ const ruBtn = document.querySelector('[data-ru]');
 const uaBtnMob = document.querySelector('[data-ua-mob]');
 const ruBtnMob = document.querySelector('[data-ru-mob]');
 
-// const overlayMenu = document.querySelector('[data-overlay]');
+const overlayForm = document.querySelector('[data-overlay]');
 // const mobileMenu = document.querySelector('.modal__btn');
 const mobileMenu = document.querySelector('.js-menu-container');
+const form = document.querySelector('.js-form-container');
 const openMenuBtn = document.querySelector('.js-open-menu');
-// const closeMenuBtn = document.querySelector('.js-close-menu');
+const openFormBtn = document.querySelector('.js-open-form');
+const closeFormBtn = document.querySelector('.js-close-form');
+
+const formValue = document.querySelector('.contactForm');
+const formHide = document.querySelector('.form__wrap');
+const formSuccess = document.querySelector('.form__success');
 
 ruBtn.addEventListener('click', () => {
   uaBtn.classList.remove('active');
@@ -34,5 +40,37 @@ window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
 });
 
 openMenuBtn.addEventListener('click', () => {
+  form.classList.remove('is__open');
+  overlayForm.classList.remove('backdrop');
   mobileMenu.classList.toggle('is__open');
+});
+
+openFormBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('is__open');
+  form.classList.toggle('is__open');
+
+  overlayForm.classList.toggle('backdrop');
+});
+
+closeFormBtn.addEventListener('click', () => {
+  form.classList.remove('is__open');
+  document.body.classList.remove('backdrop');
+  overlayForm.classList.remove('backdrop');
+});
+
+formValue.addEventListener('submit', e => {
+  e.preventDefault();
+  formHide.classList.add('send');
+  formSuccess.classList.remove('send');
+
+  setTimeout(() => {
+    form.classList.remove('is__open');
+    formSuccess.classList.add('send');
+    overlayForm.classList.toggle('backdrop');
+  }, 1000);
+
+  setTimeout(() => {
+    formValue.reset();
+    formHide.classList.remove('send');
+  }, 2000);
 });
